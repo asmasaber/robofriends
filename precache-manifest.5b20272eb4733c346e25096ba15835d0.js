e937cacae63fa74fdfbdbd7fd68f4c378756fb8d
asmasaber <asmaa.saber93@gmail.com>
2019-01-20 23:45:06 +0200

    Updates

---
+++ precache-manifest.5b20272eb4733c346e25096ba15835d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "6c032305b662d729fad8",
+    "url": "/robofriends/static/js/main.6c032305.chunk.js"
+  },
+  {
+    "revision": "ab628cf1e0d0c93d889c",
+    "url": "/robofriends/static/js/1.ab628cf1.chunk.js"
+  },
+  {
+    "revision": "6c032305b662d729fad8",
+    "url": "/robofriends/static/css/main.ee01b00d.chunk.css"
+  },
+  {
+    "revision": "ab628cf1e0d0c93d889c",
+    "url": "/robofriends/static/css/1.a466cc41.chunk.css"
+  },
+  {
+    "revision": "73b5018f5bba996878d1b3ba1b270a28",
+    "url": "/robofriends/index.html"
+  }
+];
