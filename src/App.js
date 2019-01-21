a68a91d005f4b36cb6290666473e774117191f02
asmasaber <asmaa.saber93@gmail.com>
2019-01-21 13:46:43 +0200

    seach robots with manual state mangment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots }  from './robots';
 
-const App = () => {
-    return (
-        <div>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    onSeachChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+    render() {
+        const filterRobots = this.state.robots.filter( robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div>
+                <h1>RoboFriends</h1>
+                <SearchBox seachChange = { this.onSeachChange }/>
+                <CardList robots={ filterRobots }/>
+            </div>
+        )
+    }
 }
 
 export default App;
