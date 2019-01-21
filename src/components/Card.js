81151f5b0381204ee8f6b0a97251c7015edc91e8
asmasaber <asmaa.saber93@gmail.com>
2019-01-21 17:48:40 +0200

    remove throwed error form Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Card = ({name, email, id}) => { //obj Destructing 
-    throw new Error('NOOOOOOOOOOOOOOO!');
+    // for Error Boundry Test ....
+    // throw new Error('NOOOOOOOOOOOOOOO!');
     return (
         <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
             <img alt="robot img" src={`https://robohash.org/${id}?100*100`}/>
